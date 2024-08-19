5926933db2c21d1f8d01c3c7c753e845cf563b3a
Fares <119789181+Desray19@users.noreply.github.com>
2024-08-19 10:57:08 +0300

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_params(text):
+    """"""
+    text_new = text.replace('_', ' ')
+
+    return "C {}".format(text_new)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
