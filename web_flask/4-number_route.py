155f9a47a0b4250a6df64f74acc2c9b0eadaf86e
Fares <119789181+Desray19@users.noreply.github.com>
2024-08-19 11:24:41 +0300

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_params(text):
+    """"""
+    text_new = text.replace('_', ' ')
+
+    return "C {}".format(text_new)
+
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def p_params(text):
+    """"""
+    text_new = text.replace('_', ' ')
+
+    return "Python {}".format(text_new)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
